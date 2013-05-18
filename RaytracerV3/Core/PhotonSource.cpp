d4ae2b3a0f59e9a836302e54df3ace8c87c0687c
Pascal Spörri <pascal@spoerri.io>
2013-05-18 17:12:17 +0200

    Added photon source class

---
+++ RaytracerV3/Core/PhotonSource.cpp
@@ -0,0 +1,9 @@
+//
+//  PhotonSource.cpp
+//  RaytracerV3
+//
+//  Created by Pascal Spörri on 5/18/13.
+//  Copyright (c) 2013 Pascal Spörri. All rights reserved.
+//
+
+#include "PhotonSource.h"
---
+++ RaytracerV3/Core/PhotonSource.h
@@ -0,0 +1,41 @@
+//
+//  PhotonSource.h
+//  RaytracerV3
+//
+//  Created by Pascal Spörri on 5/18/13.
+//  Copyright (c) 2013 Pascal Spörri. All rights reserved.
+//
+
+#ifndef __RaytracerV3__PhotonSource__
+#define __RaytracerV3__PhotonSource__
+
+#include "Math/Vec3.h"
+#include "Math/Color.h"
+#include <vector>
+
+class Scene;
+//#include "Scene.h"
+
+typedef struct EmittedPhoton
+{
+    Math::Vec3d position;
+    Math::Vec3f power;
+    Math::Vec3d dir;
+    bool        specularBounces;
+} EmittedPhoton;
+
+class PhotonSource
+{
+public:
+    PhotonSource(const Math::Color3f& color, double power, int photons);
+    ~PhotonSource();
+    
+    const Math::Color3f color;
+    const double power;
+    const int photons;
+    
+    virtual void emitPhotons(std::vector<EmittedPhoton>& photons, Scene& scene) = 0;
+    
+    
+};
+#endif /* defined(__RaytracerV3__PhotonSource__) */
